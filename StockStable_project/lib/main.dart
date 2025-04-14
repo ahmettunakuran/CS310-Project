e417f265d122a853c768bfc7d1b69b74ab9c18b7
İsmail Yiğit Önal <ismailyigitonal@192.168.1.8>
2025-04-15 00:52:52 +0300

    Saved my own changes

--- StockStable_project/lib/main.dart
+++ StockStable_project/lib/main.dart
@@ -8,8 +8,9 @@ import 'screens/inventory_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/sign_up_screen.dart';
 import 'screens/forgot_password_screen.dart';
-import 'screens/splash_screen.dart'; 
-
+import 'screens/splash_screen.dart';
+import 'screens/delete_product_screen.dart';
+import 'screens/order_history_screen.dart';
 
 void main() {
   runApp(const StockStableApp());
@@ -35,12 +36,16 @@ class StockStableApp extends StatelessWidget {
         '/barcodeScanner': (context) => const BarcodeScannerScreen(),
         '/addItem': (context) => const AddItemScreen(),
         '/inventory': (context) => const InventoryScreen(),
-        // TODO: FURAN: SettingsScreen.dart oluşturup designladıktna sonra sağdaki navigasyonu buraya yapıştırılıcak -> '/settings': (context) => const SettingsScreen(),
-        // TODO: OZAN: HelpScreen.dart oluşturup designladıktna sonra sağdaki navigasyonu buraya yapıştırılıcak -> '/help': (context) => const HelpScreen(),
-        // TODO: OZAN: ProductDetailsScreen.dart oluşturup designladıktna sonra sağdaki navigasyonu buraya yapıştırılıcak -> '/prodcutDetails': (context) => const ProdcutDetailsScreen(),
-        // TODO: TUNA: OrderHistoryScreen.dart oluşturup designladıktna sonra sağdaki navigasyonu buraya yapıştırılıcak -> '/orderHistory': (context) => const OrderHistoryScreen(),
-        // TODO: TUNA: DeleteProductScreen.dart oluşturup designladıktna sonra sağdaki navigasyonu buraya yapıştırılıcak -> '/deleteProduct': (context) => const DeleteProductScreen(),
+        '/deleteItem': (context) => const DeleteProductScreen(), // senin özel route’un
+        '/deleteProduct': (context) => const InventoryScreen(),   // dikkatli kullan
+        '/orderHistory': (context) => const OrderHistoryScreen(),
+
+        // TODO:
+        // '/settings': (context) => const SettingsScreen(),
+        // '/help': (context) => const HelpScreen(),
+        // '/prodcutDetails': (context) => const ProdcutDetailsScreen(),
       },
     );
   }
 }
+
