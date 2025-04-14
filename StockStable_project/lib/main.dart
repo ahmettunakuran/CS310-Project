7dab0a2002b368a41bcedbc4b2c4e1a81678a1a5
Ozan <ozan.tariyan@sabanciuniv.edu>
2025-04-14 20:44:24 +0300

    Added Inventory page and Add_item page

--- StockStable_project/lib/main.dart
+++ StockStable_project/lib/main.dart
@@ -3,6 +3,8 @@ import 'screens/home_screen.dart';
 import 'screens/out_of_stock_screen.dart';
 import 'screens/stock_information_screen.dart';
 import 'screens/barcode_scanner_screen.dart';
+import 'screens/add_item_screen.dart';
+import 'screens/inventory_screen.dart';
 
 void main() {
   runApp(const StockStableApp());
@@ -21,13 +23,8 @@ class StockStableApp extends StatelessWidget {
         '/outOfStock': (context) => const OutOfStockScreen(),
         '/stockInfo': (context) => const StockInformationScreen(),
         '/barcodeScanner': (context) => const BarcodeScannerScreen(),
-        // TODO: FURAN: SettingsScreen.dart oluşturup designladıktna sonra sağdaki navigasyonu buraya yapıştırılıcak -> '/settings': (context) => const SettingsScreen(),
-        // TODO: OZAN: HelpScreen.dart oluşturup designladıktna sonra sağdaki navigasyonu buraya yapıştırılıcak -> '/help': (context) => const HelpScreen(),
-        // TODO: OZAN: AddItemScreen.dart oluşturup designladıktna sonra sağdaki navigasyonu buraya yapıştırılıcak -> '/addItem': (context) => const AddItemScreen(),
-        // TODO: OZAN: InventoryScreen.dart oluşturup designladıktna sonra sağdaki navigasyonu buraya yapıştırılıcak -> '/inventory': (context) => const InventoryScreen(),
-        // TODO: OZAN: ProductDetailsScreen.dart oluşturup designladıktna sonra sağdaki navigasyonu buraya yapıştırılıcak -> '/prodcutDetails': (context) => const ProdcutDetailsScreen(),
-        // TODO: TUNA: OrderHistoryScreen.dart oluşturup designladıktna sonra sağdaki navigasyonu buraya yapıştırılıcak -> '/orderHistory': (context) => const OrderHistoryScreen(),
-        // TODO: TUNA: DeleteProductScreen.dart oluşturup designladıktna sonra sağdaki navigasyonu buraya yapıştırılıcak -> '/deleteProduct': (context) => const DeleteProductScreen(),
+        '/addItem': (context) => const AddItemScreen(),
+        '/inventory': (context) => const InventoryScreen(),
       },
     );
   }
