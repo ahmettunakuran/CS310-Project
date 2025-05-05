e6e155b94f5cd51937982872e5f68dc8256a92c8
Ozan <ozan.tariyan@sabanciuniv.edu>
2025-05-05 18:46:38 +0300

    added user authentication

--- StockStable_project/lib/main.dart
+++ StockStable_project/lib/main.dart
@@ -14,8 +14,14 @@ import 'screens/delete_product_screen.dart';
 import 'screens/order_history_screen.dart';
 import 'screens/help_screen.dart';
 import 'screens/settings_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const StockStableApp());
 }
 
