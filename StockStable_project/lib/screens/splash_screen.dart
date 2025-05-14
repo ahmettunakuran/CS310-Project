828fa8e4debd20aad734f68e6d2a7f480f70c4d1
Ozan <ozan.tariyan@sabanciuniv.edu>
2025-05-14 16:30:02 +0300

    Added web page login

--- StockStable_project/lib/screens/splash_screen.dart
+++ StockStable_project/lib/screens/splash_screen.dart
@@ -1,27 +1,34 @@
 import 'package:flutter/material.dart';
-
 import 'package:provider/provider.dart';
 import '../providers/auth_provider.dart';
-
+import '../utils/app_colors.dart';
+import '../utils/app_padding.dart';
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // AuthProvider’daki durum
     final isLoggedIn = context.watch<AuthProvider>().isLoggedIn;
 
-    // Build tamamlandıktan hemen sonra yönlendirme yap
-    Future.microtask(() {
+    // 1.5 saniye sonra yönlendirme yap
+    Future.delayed(const Duration(seconds: 2), () {
       Navigator.pushReplacementNamed(
         context,
         isLoggedIn ? '/home' : '/login',
       );
     });
 
-    // Kullanıcı yönlendirilene kadar basit bir yükleme animasyonu göster
-    return const Scaffold(
-      body: Center(child: CircularProgressIndicator()),
+    return Scaffold(
+      backgroundColor: AppColors.backgroundWhite,
+      body: Center(
+        child: Padding(
+          padding: AppPadding.all16,
+          child: Image.asset(
+            'lib/assets/stock_stable_logo.png',
+            width: 200,
+          ),
+        ),
+      ),
     );
   }
 }
